69e071bc21e31dc7f1bad767cde6b91aa21fdfa3
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-05-07 11:38:15 -0400

    Switch back to `youtube`
    I was using a version of `yt-dlp` from 2023, causing errors.

--- utils/fs/youtube.go
+++ utils/fs/youtube.go
@@ -1,79 +1,51 @@
 package fs
 
 import (
-	"context"
 	"fmt"
 	"gobot/models"
 	"io"
-	"log"
 	"os"
 
-	"github.com/wader/goutubedl"
+	"github.com/kkdai/youtube/v2"
 )
 
 const AUDIO_FOLDER string = "audio_temp"
 
 func DownloadYoutubeURLToFile(url string, folder string) (*models.SongInfo, error) {
-	result, err := goutubedl.New(context.Background(), url, goutubedl.Options{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	downloadResult, err := result.Download(context.Background(), "")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer downloadResult.Close()
 
-	filePath := fmt.Sprintf("%s/%s", AUDIO_FOLDER, result.Info.Title)
+	client := youtube.Client{}
+
+	video, err := client.GetVideo(url)
+	if err != nil {
+		panic(err)
+	}
+
+	filePath := fmt.Sprintf("%s/%s", AUDIO_FOLDER, video.Title)
+
 	songInfo := models.SongInfo{
 		FilePath: filePath,
 
-		Title:    result.Info.Title,
-		Uploader: result.Info.Uploader,
+		Title:    video.Title,
+		Uploader: video.Author,
 	}
 
-	f, err := os.Create(filePath)
-	fmt.Println(filePath)
+	formats := video.Formats.WithAudioChannels() // only get videos with audio
+	stream, _, err := client.GetStream(video, &formats[0])
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	defer f.Close()
-	io.Copy(f, downloadResult)
+	defer stream.Close()
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		panic(err)
+	}
 
 	return &songInfo, nil
-	//	client := youtube.Client{}
-	//
-	//	video, err := client.GetVideo(url)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-
-	//	filePath := fmt.Sprintf("%s/%s", AUDIO_FOLDER, video.Title)
-
-	//	songInfo := models.SongInfo{
-	//		FilePath: filePath,
-	//
-	//		Title:    video.Title,
-	//		Uploader: video.Author,
-	//	}
-	//
-	//	formats := video.Formats.WithAudioChannels() // only get videos with audio
-	//	stream, _, err := client.GetStream(video, &formats[0])
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	defer stream.Close()
-	//
-	//	file, err := os.Create(filePath)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	defer file.Close()
-	//
-	//	_, err = io.Copy(file, stream)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//
-	//	return &songInfo, nil
 }
