072d3dcef42d0ed0e79d607f38fe5638e7897b06
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-05-07 03:44:41 -0400

    Remove member info command

--- commands/commands.go
+++ commands/commands.go
@@ -8,7 +8,6 @@ var AllCommands = map[string]models.Command{
 	HelpCommand.Name:       HelpCommand,
 	PingCommand.Name:       PingCommand,
 	OwnersListCommand.Name: OwnersListCommand,
-	MemberInfoCommand.Name: MemberInfoCommand,
 	PlayCommand.Name:       PlayCommand,
 	StopCommand.Name:       StopCommand,
 }
