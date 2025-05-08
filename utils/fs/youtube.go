c662e4308716441493e6a6056714faf5b157f315
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-05-08 19:47:45 -0400

    I did that wrong

--- utils/fs/youtube.go
+++ utils/fs/youtube.go
@@ -19,70 +19,64 @@ func RemoveDownloadedSong(song models.SongInfo) {
 }
 
 func DownloadYoutubeURLToFile(url string, folder string) (*models.SongInfo, error) {
-	downloadOptions := new(goutubedl.DownloadOptions)
-	downloadOptions.DownloadAudioOnly = true
-	downloadOptions.AudioFormats = "best"
 	goutubeOptions := new(goutubedl.Options)
 	goutubeOptions.DownloadThumbnail = false
 	goutubeOptions.DownloadSubtitles = false
-	if _, err := os.Stat("history.txt"); errors.Is(err, os.ErrNotExist) {
-		result, err := goutubedl.New(context.Background(), url, *goutubeOptions)
-		if err != nil {
-			log.Fatal(err)
-		}
+	result, err := goutubedl.New(context.Background(), url, *goutubeOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// check if we havent downloaded it
+	if _, err := os.Stat(fmt.Sprintf("%s/%s", AUDIO_FOLDER, result.Info.ID)); errors.Is(err, os.ErrNotExist) {
+		filePath := fmt.Sprintf("%s/%s", AUDIO_FOLDER, result.Info.ID)
+
+		downloadOptions := new(goutubedl.DownloadOptions)
+		downloadOptions.DownloadAudioOnly = true
+		downloadOptions.AudioFormats = "best"
 		downloadResult, err := result.DownloadWithOptions(context.Background(), *downloadOptions)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer downloadResult.Close()
-
-		filePath := fmt.Sprintf("%s/%s", AUDIO_FOLDER, result.Info.ID)
-		songInfo := models.SongInfo{
-			FilePath: filePath,
-
-			Title:    result.Info.Title,
-			Uploader: result.Info.Uploader,
-			ID:       result.Info.ID,
-		}
-
 		f, err := os.Create(filePath)
-		fmt.Println(filePath)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer f.Close()
-		cwd, err := os.Getwd()
-		if err != nil {
-			panic("Cant get CWD")
-		}
-		lf, err := os.OpenFile(cwd+"/history.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		if err != nil {
-			panic(err)
-		}
-		defer lf.Close()
 
-		// Log the ID so we dont have to download it again
-		if _, err = lf.WriteString(songInfo.ID); err != nil {
-			panic(err)
-		}
 		io.Copy(f, downloadResult)
-		return &songInfo, nil
 
-	} else {
-		result, err := goutubedl.New(context.Background(), url, *goutubeOptions)
-		if err != nil {
-			log.Fatal(err)
-		}
-		filePath := fmt.Sprintf("%s/%s", AUDIO_FOLDER, result.Info.ID)
-		songInfo := models.SongInfo{
-			FilePath: filePath,
-
-			Title:    result.Info.Title,
-			Uploader: result.Info.Uploader,
-			ID:       result.Info.ID,
-		}
-		return &songInfo, nil
 	}
+
+	filePath := fmt.Sprintf("%s/%s", AUDIO_FOLDER, result.Info.ID)
+	songInfo := models.SongInfo{
+		FilePath: filePath,
+
+		Title:    result.Info.Title,
+		Uploader: result.Info.Uploader,
+		ID:       result.Info.ID,
+	}
+
+	fmt.Println(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic("Cant get CWD")
+	}
+	lf, err := os.OpenFile(cwd+"/history.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer lf.Close()
+
+	// Log the ID so we dont have to download it again
+	if _, err = lf.WriteString(songInfo.ID); err != nil {
+		panic(err)
+	}
+
+	return &songInfo, nil
 	//	client := youtube.Client{}
 	//
 	//	video, err := client.GetVideo(url)
