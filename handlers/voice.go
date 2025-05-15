4e67e96f634a1fac27071916520a26c3c3d5f182
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-05-15 15:33:19 -0400

    Fix `playNextSongInQueue` to work with WS

--- handlers/voice.go
+++ handlers/voice.go
@@ -226,6 +226,9 @@ func playNextSongInQueue(v *discordgo.VoiceConnection, ctx *models.Context, stop
 		fmt.Println(ctx.Client.SongQueue[0])
 
 		var song *models.SongInfo = ctx.Client.SongQueue[0]
+		url := "https://www.youtube.com/watch?v=" + song.ID // Build the URL for the WS server
+
+		ctx.Client.SendDownloadToWS(url) // Send to the WS server to play
 		PlayFromWS(v, ctx, song, stop, skip, seek)
 	}
 }
